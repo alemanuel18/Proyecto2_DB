89c722f5ae775f44c57cc33a6a7b469bbda3aac2
Alemanuel <alemanuelj5@gmail.com>
2026-05-13 09:20:58 -0600

    ultimo fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true
+  server: {
+    allowedHosts: [
+      process.env.VITE_ALLOWED_HOST || 'localhost'
+    ]
   }
 })
